0f12e4ccb77edab58acda6dcf18ff52373a4fab2
Maik Rebaum <maikrebaum@gmail.com>
2023-12-01 00:53:03 +0100

    write all data into massive csv

--- tools/lollipop/src/main.rs
+++ tools/lollipop/src/main.rs
@@ -1,14 +1,11 @@
 use clap::Parser;
 use color_eyre::eyre::{Result, WrapErr};
+use csv::Writer;
+use flatten_json_object::Flattener;
 use hula_types::RobotState;
-use log::{info, LevelFilter};
-use polars::prelude::*;
+use log::{debug, LevelFilter};
 use rmp_serde::from_slice;
-use serde_json;
-use std::{
-    fs::File,
-    io::{self, Read},
-};
+use std::{fs::File, io::Read};
 const LOLA_BUFF_SIZE: usize = 896;
 //const HULA_BUFF_SIZE: usize = 786;
 #[derive(Parser, Debug)]
@@ -34,50 +31,55 @@ fn main() -> Result<()> {
             },
         )
         .init();
+    let mut filewriter = Writer::from_path("/home/maik/LOLADATA/lola30.csv")?;
     let mut file = File::open("/home/maik/LOLADATA/lola30").wrap_err("Failed to open file")?;
-    //let csv_file = File::create("kneepitch.csv").wrap_err("Could not create csv file")?;
+    //let mut csv_file = File::create("kneepitch.csv").wrap_err("Could not create csv file")?;
     //csv_file.write_all(b"timestamp,leftkneepitch,rightkneepitch\n")?;
-    let dataframe = DataFrame::default();
     let mut timestamp_buf = [0; 16];
+    let timestamp = u128::from_be_bytes(timestamp_buf);
     file.read_exact(&mut timestamp_buf)
         .wrap_err("Could not read first time stamp")?;
     let mut robot_state =
         read_lola_message(&mut file).wrap_err("Failed to read first lola message")?;
-    robot_state.received_at = 1000.0;
-    let jsonstring = serde_json::to_value(robot_state).wrap_err("Could not convert to json")?;
-    info!("{}", jsonstring);
+    robot_state.received_at = timestamp as f32;
+    let jsonobject = serde_json::to_value(robot_state).wrap_err("Could not convert to json")?;
+    let flattener = Flattener::new().set_key_separator(".");
+    let output = flattener
+        .flatten(&jsonobject)
+        .wrap_err("Failed to flatten")?;
+    let keys = output
+        .as_object()
+        .into_iter()
+        .flatten()
+        .map(|(key, _value)| (key.to_string()))
+        .collect::<Vec<_>>();
+    let values = output
+        .as_object()
+        .into_iter()
+        .flatten()
+        .map(|(_key, value)| (format!("{}", value.as_f64().unwrap())))
+        .collect::<Vec<_>>();
+    debug!("{:?}", keys);
+    filewriter.write_record(keys)?;
+    filewriter.write_record(values)?;
     //print!("{}", robot_state);
-    /*
     while let Ok(()) = file.read_exact(&mut timestamp_buf) {
         let timestamp = u128::from_be_bytes(timestamp_buf);
-        let robot_state = read_lola_message(&mut file).wrap_err("failed to read lola message")?;
-        //serde serialize to csv or json, polars dataframe (pandas in cool und in rust)
-        /*
-        let stiffness = robot_state.stiffness.into_lola();
-        let positions = robot_state.position.into_lola();
-        let temperature = robot_state.temperature.into_lola();
-        let current = robot_state.current.into_lola();
-        let battery_charge = robot_state.battery.charge;
-        let battery_current = robot_state.battery.current;
-        let battery_temperature = robot_state.battery.temperature;
-        */
-        //let accelerometer_x = robot_state.inertial_measurement_unit.accelerometer.x;
-        /*
-        let accelerometer_y = robot_state.inertial_measurement_unit.accelerometer.y;
-        let accelerometer_z = robot_state.inertial_measurement_unit.accelerometer.z;
-        let imu_angles_x = robot_state.inertial_measurement_unit.angles.x;
-        let imu_angles_y = robot_state.inertial_measurement_unit.angles.y;
-        let gyroscope_x = robot_state.inertial_measurement_unit.gyroscope.x;
-        let gyroscope_y = robot_state.inertial_measurement_unit.gyroscope.y;
-        let gyroscope_z = robot_state.inertial_measurement_unit.gyroscope.z;
-        let fsr = robot_state.force_sensitive_resistors;
-        */
-
-        //csv_file
-        //  .write_all(line.as_bytes())
-        //.wrap_err("Could not write to file")?;
+        let mut robot_state =
+            read_lola_message(&mut file).wrap_err("failed to read lola message")?;
+        robot_state.received_at = timestamp as f32;
+        let jsonobject = serde_json::to_value(robot_state).wrap_err("Could not convert to json")?;
+        let output = flattener
+            .flatten(&jsonobject)
+            .wrap_err("Failed to flatten")?;
+        let values = output
+            .as_object()
+            .into_iter()
+            .flatten()
+            .map(|(_key, value)| (format!("{}", value.as_f64().unwrap())))
+            .collect::<Vec<_>>();
+        filewriter.write_record(values)?;
     }
-    */
 
     Ok(())
 }
