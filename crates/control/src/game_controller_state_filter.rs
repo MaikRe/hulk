17bf4f639399bf5fe570133d9f641f07ab93e37c
Maik Rebaum <maikrebaum@gmail.com>
2024-07-01 17:49:47 +0200

    output whistle in set ball position

--- crates/control/src/game_controller_state_filter.rs
+++ crates/control/src/game_controller_state_filter.rs
@@ -3,7 +3,7 @@ use std::time::{Duration, SystemTime};
 use color_eyre::Result;
 use context_attribute::context;
 use coordinate_systems::{Field, Ground};
-use framework::{AdditionalOutput, MainOutput};
+use framework::MainOutput;
 use linear_algebra::{distance, Isometry2, Point2, Vector2};
 use serde::{Deserialize, Serialize};
 use spl_network_messages::{GamePhase, GameState, Team};
@@ -34,14 +34,12 @@ pub struct CycleContext {
     field_dimensions: Parameter<FieldDimensions, "field_dimensions">,
 
     ground_to_field: CyclerState<Isometry2<Ground, Field>, "ground_to_field">,
-
-    whistle_in_set_ball_position:
-        AdditionalOutput<Option<Point2<Field>>, "whistle_in_set_ball_position">,
 }
 
 #[context]
 pub struct MainOutputs {
     pub filtered_game_controller_state: MainOutput<Option<FilteredGameControllerState>>,
+    pub whistle_in_set_ball_position: MainOutput<Option<Point2<Ground>>>,
 }
 
 impl GameControllerStateFilter {
@@ -53,7 +51,7 @@ impl GameControllerStateFilter {
         })
     }
 
-    pub fn cycle(&mut self, mut context: CycleContext) -> Result<MainOutputs> {
+    pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
         let game_states = self.filter_game_states(
             *context.ground_to_field,
             context.ball_position,
@@ -78,12 +76,13 @@ impl GameControllerStateFilter {
                 .game_controller_state
                 .hulks_team_is_home_after_coin_toss,
         };
-        context
-            .whistle_in_set_ball_position
-            .fill_if_subscribed(|| self.whistle_in_set_ball_position);
 
         Ok(MainOutputs {
             filtered_game_controller_state: Some(filtered_game_controller_state).into(),
+            whistle_in_set_ball_position: self
+                .whistle_in_set_ball_position
+                .map(|ball_position| context.ground_to_field.inverse() * ball_position)
+                .into(),
         })
     }
 
